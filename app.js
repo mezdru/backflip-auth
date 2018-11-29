fa5c0f2da91e29110eb796298c9efc613d65c2b4
Quentin Drumez <quentin.drumez@isen.yncrea.fr>
2018-11-29 17:10:34 +0100

    Add authorization in header exceptions (#14)

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(function(req, res, next) {
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   //intercepts OPTIONS method
   if ('OPTIONS' === req.method) {
     //respond with 200
