835228612ed25a9e83807c02ac8bb424467d1532
Quentin Drumez <quentin.drumez@isen.yncrea.fr>
2019-11-25 10:20:17 +0100

    Update client auth helper: error log (#84)

--- helpers/client_auth_helper.js
+++ helpers/client_auth_helper.js
@@ -16,8 +16,8 @@ class ClientAuthHelper {
 					scope: 'agenda'
 				}
 			}, (error, requestResponse, body) => {
-				if (error || (body && body.status && body.status !== 200) || (requestResponse.statusCode !== 200)) {
-					console.log('error in accesstoken: ' + requestResponse.statusCode + JSON.stringify(body));
+				if (error || (body && body.status && body.status !== 200) || (requestResponse && requestResponse.statusCode !== 200)) {
+					console.log('error in accesstoken: ', error);
 					return reject(error);
 				}
 				return resolve(body.access_token);
